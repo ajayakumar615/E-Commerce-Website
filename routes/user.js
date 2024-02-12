ba650a13b99aca88c2f516768890367621fc65c5
P.Ajayakumar <pajayakumar615@gmail.com>
2024-02-12 12:25:56 +0530

    Second commit

---
+++ routes/user.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+const productHelpers = require('../helpers/product-helpers');
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    productHelpers.getAllProducts().then((products) => {
+        console.log(products);
+        res.render('user/view-products', { products })
+    })
+});
+module.exports = router;
